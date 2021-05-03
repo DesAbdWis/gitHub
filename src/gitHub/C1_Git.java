c5a60fadbb615e6dcda21de8b2931989d2e670bb
serefahmet21 <81472020+serefahmet21@users.noreply.github.com>
2021-05-03 22:58:57 +0300

    Update C1_Git.java

--- src/gitHub/C1_Git.java
+++ src/gitHub/C1_Git.java
@@ -3,8 +3,8 @@ package gitHub;
 public class C1_Git {
 
 	public static void main(String[] args) {
-		System.out.println("merhaba Dunya")
-	        System.out.println("hoscakal Dunya")
+		System.out.println("merhaba Dunya");
+	        System.out.println("hoscakal Dunya");
 
 	}
 
